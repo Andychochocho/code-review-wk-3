54cccaa653c1c70f98e235f79e7a73b1b115ea4f
Andrew Cho <cho-andrew@hotmail.com>
2016-01-22 13:33:40 -0800

    add jquery so user can submit their input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-var pingPong = function(number) {
+function pingPong(number) {
   var ping = "ping"
   var pong = "pong"
-  var pingpong = "pingpong"
+  var pongPing = "pingpong"
   var numberThree = 3
   var numberFive = 5
   var arrayOutput = [];
 
   for (var i = 1; i <= number; i++) {
-    if (i % numberThree === 0 && (i % numberFive === 0)) {
-      arrayOutput.push(pingpong);
+    if (i % numberThree === 0 && i % numberFive === 0) {
+      arrayOutput.push(pongPing);
     } else if (i % numberThree === 0) {
       arrayOutput.push(ping);
     } else if (i % numberFive === 0) {
@@ -18,4 +18,14 @@ var pingPong = function(number) {
       }
   }
   return arrayOutput;
-};
+  };
+
+$(function() {
+  $("form#pingPong").submit(function(event){
+    var userInput = parseInt($("#userInput").val());
+    var result = pingPong(userInput);
+    $('#result').append(result);
+    $('#result').show();
+    event.preventDefault();
+  });
+});
