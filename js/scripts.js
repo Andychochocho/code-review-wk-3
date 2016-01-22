f160a3eac77d0b0f7c6c58c84eade6d8985f3fdc
Andrew Cho <cho-andrew@hotmail.com>
2016-01-22 09:46:54 -0800

    add variable name for pingpong and change array push parameters to be variable names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 var pingPong = function(number) {
   var ping = "ping"
   var pong = "pong"
+  var pingpong = "pingpong"
   var numberThree = 3
   var numberFive = 5
   var arrayOutput = [];
 
   for (var i = 1; i <= number; i++) {
     if (i % numberThree === 0 && (i % numberFive === 0)) {
-      arrayOutput.push("pingpong");
+      arrayOutput.push(pingpong);
     } else if (i % numberThree === 0) {
-      arrayOutput.push("ping");
+      arrayOutput.push(ping);
     } else if (i % numberFive === 0) {
-      arrayOutput.push("pong");
+      arrayOutput.push(pong);
     } else {
       arrayOutput.push(i);
       }
