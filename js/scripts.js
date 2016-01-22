c8bdd5af27f0cbd2fd0d384047e7b51f60ee7979
Andrew Cho <cho-andrew@hotmail.com>
2016-01-22 09:29:12 -0800

    add function to change numbers divisible by five to pong.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 var pingPong = function(number) {
   var ping = "ping"
+  var pong = "pong"
   var numberThree = 3
+  var numberFive = 5
   var arrayOutput = [];
 
   for (var i = 1; i <= number; i++) {
     if (i % numberThree === 0) {
     arrayOutput.push("ping");
-    } else {
+  } else if (i % numberFive === 0) {
+    arrayOutput.push("pong");
+  } else {
       arrayOutput.push(i);
       }
   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,7 @@
 describe("pingPong",function (){
+  it("will change all returned numbers divisible by five to pong", function(){
+    expect(pingPong(5)).to.eql([1, 2, "ping", 4, 'pong']);
+  })
   it("will change all returned numbers divisible by three to ping", function(){
     expect(pingPong(3)).to.eql([1,2,"ping"]);
   });
